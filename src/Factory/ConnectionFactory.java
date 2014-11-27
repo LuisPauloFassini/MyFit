5fc3251490b8b04f8db067910dffbe65683de5b1
Prog03 <Prog03@GSPROG03.gs.local>
2014-11-27 13:15:55 -0200

    Primeira versão.

---
+++ src/Factory/ConnectionFactory.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Luís Paulo
+ */
+public class ConnectionFactory {
+    
+    public static Connection getConnection() {
+        try {
+            //Registra JDBC driver
+           Class.forName("com.mysql.jdbc.Driver");
+
+           //Abrindo a conexão
+           return DriverManager.getConnection("jdbc:mysql://localhost:3306/myFit", "root", "");   
+        } catch (ClassNotFoundException | SQLException e) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return null;
+    }
+    
+}
